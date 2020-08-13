e8963a5c6ae0ad6c5a66a4594b6c000dbc29fd81
nickgayo12 <nickgayo12@gmail.com>
2020-08-13 20:26:59 +0800

    chore: initial commit

---
+++ pages/index.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-container class="home__container mx-auto">
+    <v-row justify="center" align="center">
+      <v-img src="/icon.png" class="mt-10" contain height="300" width="300" />
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Component, Vue, namespace } from 'nuxt-property-decorator'
+
+const GLOBAL_STORE = namespace('global')
+
+@Component({
+  head() {
+    return {
+      title: 'Home'
+    }
+  }
+})
+export default class Index extends Vue {
+  @GLOBAL_STORE.Action('setTitle') global_set_title!: (payload: string) => void
+}
+</script>
+
+<style scoped>
+.home__container {
+  max-width: 1440px;
+}
+</style>
---
+++ types/repository.d.ts
@@ -0,0 +1,10 @@
+import { UserResponseInterface } from '~/api/social'
+
+declare module 'vue/types/vue' {
+  interface Vue {
+    $socialRepository: {
+      GetUsers: () => Promise<UserResponseInterface[]>
+      GetUserByID: (id: number) => Promise<UserResponseInterface>
+    }
+  }
+}
