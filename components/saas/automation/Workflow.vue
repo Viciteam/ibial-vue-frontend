dd2e800fe89931a6d0cba0ab3ffb8723ed0bf927
Rocky Gonzales <gonzrock12@gmail.com>
2020-09-19 08:57:56 +0800

    Update Workflow.vue

--- components/saas/automation/Workflow.vue
+++ components/saas/automation/Workflow.vue
@@ -5,7 +5,13 @@
         <v-col cols="5">
           <v-row>
             <v-col cols="2" class="py-0">
-              <v-checkbox class="mt-2 pl-3" hide-details value></v-checkbox>
+              <v-checkbox
+                v-model="selectAll"
+                class="mt-2 pl-3"
+                hide-details
+                value
+                @click.native.stop="selectedAll()"
+              ></v-checkbox>
             </v-col>
             <v-col cols="10" class="pl-8">
               Workflow
@@ -32,6 +38,7 @@
           <v-row>
             <v-col v-if="isListView" cols="1">
               <v-checkbox
+                v-model="checkbox[n]"
                 class="mt-n1 pr-0 pl-3"
                 hide-details
                 value
@@ -126,12 +133,33 @@ export default {
           apps: ['mdi-file-document', 'mdi-file-document', 'mdi-file'],
           isactive: true
         }
-      ]
+      ],
+      checkbox: {},
+      selectAll: false,
+      count: 8
     }
   },
   computed: {
     isListView() {
       return this.views
+    }
+  },
+  mounted() {
+    /* this is just example, in this case you could type number properties 
+    directly in data.checkbox like checkbox[0] = false etc., but in a real case 
+    you may want to use string ID or any API based index with it's corresponding 
+    v-for html markdown */
+    for (let i = 0; i < 8; i++) {
+      // maintaining reactivity
+      this.$set(this.checkbox, i, false)
+    }
+  },
+  methods: {
+    selectedAll() {
+      for (const i in this.checkbox) {
+        console.log(this.selectAll)
+        this.checkbox[i] = this.selectAll
+      }
     }
   }
 }
