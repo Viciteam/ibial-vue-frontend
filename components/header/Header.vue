875e73e3ddc09a0d8dcad494206c3ab75c58024d
edwardethan <eduardo_arboleda@umindanao.edu.ph>
2020-10-19 15:03:16 +0800

    full-header done

--- components/header/Header.vue
+++ components/header/Header.vue
@@ -2,56 +2,77 @@
   <v-app-bar app flat clipped-left align="center" color="white">
     <img src="/logo/ibial_logo_big.png" width="24px" />
     <v-spacer></v-spacer>
-    <v-tabs flat icons-and-text>
-      <v-tabs-slider></v-tabs-slider>
-      <v-tab class="text-capitalize" href="#tab-1">
+    <v-tabs v-model="tabs" icons-and-text centered>
+      <v-tabs-slider class="d-none"></v-tabs-slider>
+      <v-tab class="text-capitalize px-10" href="#tab-1">
         Tools
-        <img src="/icons/header/tools_idle.svg" width="20px" />
+        <img
+          :src="'/icons/header/tools_' + activeIcon(tabs, 1) + '.svg'"
+          width="20px"
+        />
       </v-tab>
-      <v-tab class="text-capitalize" href="#tab-2">
+      <v-tab class="text-capitalize px-10" href="#tab-2">
         Team Space
-        <img src="/icons/header/team_idle.svg" width="20px" />
+        <img
+          :src="'/icons/header/team_' + activeIcon(tabs, 2) + '.svg'"
+          width="20px"
+          alt=""
+        />
       </v-tab>
-      <v-tab class="text-capitalize" href="#tab-3">
+      <v-tab class="text-capitalize px-10" href="#tab-3">
         Community
-        <img src="/icons/header/community_idle.svg" width="20px" />
+        <img
+          :src="'/icons/header/community_' + activeIcon(tabs, 3) + '.svg'"
+          width="20px"
+        />
       </v-tab>
-      <v-tab class="text-capitalize" href="#tab-4">
+      <v-tab class="text-capitalize px-10" href="#tab-4">
         Store
-        <img src="/icons/header/market_idle.svg" width="20px" />
+        <img
+          :src="'/icons/header/market_' + activeIcon(tabs, 4) + '.svg'"
+          width="20px"
+        />
       </v-tab>
     </v-tabs>
     <v-spacer></v-spacer>
-    <v-text-field
-      label="Search"
-      solo
-      flat
-      hide-details
-      background-color="light_gray"
-      prepend-inner-icon="mdi-magnify"
-    ></v-text-field>
-
-    <v-btn icon>
-      <img src="/icons/header/cart_idle.svg" width="20px" />
+    <v-card flat width="400px" class="mr-5">
+      <v-text-field
+        label="Search"
+        solo
+        flat
+        hide-details
+        background-color="light_gray"
+        prepend-inner-icon="mdi-magnify"
+      ></v-text-field>
+    </v-card>
+    <v-btn icon class="mx-1">
+      <v-badge content="1" color="error">
+        <img src="/icons/header/cart_idle.svg" width="20px" />
+      </v-badge>
     </v-btn>
-    <v-btn icon>
-      <img src="/icons/header/message_idle.svg" width="20px" />
+    <v-btn icon class="mx-1">
+      <v-badge content="5" color="error">
+        <img src="/icons/header/message_idle.svg" width="20px" />
+      </v-badge>
     </v-btn>
-    <v-btn icon>
-      <img src="/icons/header/bell_idle.svg" width="20px" />
+    <v-btn icon class="mx-1">
+      <v-badge content="99+" color="error">
+        <img src="/icons/header/bell_idle.svg" width="20px" />
+      </v-badge>
     </v-btn>
-    <v-avatar>
+    <v-avatar class="ml-5">
       <img src="https://cdn.vuetifyjs.com/images/john.jpg" width="20px" />
     </v-avatar>
-    <v-select
-      solo
-      flat
-      hide-details
-      background-color="transparent"
-      label="Business Name #1"
-    >
-    </v-select>
-
+    <v-card flat width="350px">
+      <v-select
+        solo
+        flat
+        hide-details
+        background-color="transparent"
+        label="Ibial Solution"
+      >
+      </v-select>
+    </v-card>
     <span v-if="this.$auth.loggedIn" @click="logout">logout</span>
   </v-app-bar>
 </template>
@@ -60,16 +81,10 @@
 export default {
   data() {
     return {
-      /**
-       * all the datas here
-       */
+      tabs: 0
     }
   },
-  computed: {
-    /**
-     * all the computed here
-     */
-  },
+  computed: {},
   watch: {
     /**
      * all the watchers here
@@ -81,9 +96,13 @@ export default {
      */
   },
   methods: {
-    /**
-     * all the methods here
-     */
+    activeIcon(num, tab) {
+      if (num === 'tab-' + tab) {
+        return 'active'
+      } else {
+        return 'idle'
+      }
+    },
     logout() {
       this.$auth.logout()
     }
