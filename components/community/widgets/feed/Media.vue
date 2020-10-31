3ddeb8b40b8986eb56e9aa8ad11f5706066e7585
nickgayo12 <nickgayo12@gmail.com>
2020-10-31 13:21:10 +0800

    fix(wip): preview image, layout

---
+++ components/community/widgets/feed/Media.vue
@@ -0,0 +1,47 @@
+<template>
+  <v-row no-gutters>
+    <v-col
+      v-for="(item, n) in media"
+      :key="n"
+      :cols="
+        media.length === 1
+          ? '12'
+          : media.length === 2
+          ? '6'
+          : media.length === 3
+          ? n === 2
+            ? '12'
+            : '6'
+          : media.length === 4
+          ? '6'
+          : '6'
+      "
+      class="column-container--padding"
+    >
+      <v-img
+        max-height="240"
+        :src="`https://picsum.photos/500/300?image=${n * 5 + 10}`"
+        :lazy-src="`https://picsum.photos/10/6?image=${n * 5 + 10}`"
+        aspect-ratio="1"
+        class="grey lighten-2"
+      ></v-img>
+    </v-col>
+  </v-row>
+</template>
+
+<script>
+export default {
+  props: {
+    media: {
+      type: Array,
+      default: () => []
+    }
+  }
+}
+</script>
+
+<style scoped>
+.column-container--padding {
+  padding: 1px;
+}
+</style>
