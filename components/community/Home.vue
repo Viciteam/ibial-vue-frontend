01f22673ff153050d7ebdb0a4fc9a1e9aa18fbd1
Rocky Gonzales <gonzrock12@gmail.com>
2020-11-06 10:37:37 +0800

    adding static version homepage

--- components/community/Home.vue
+++ components/community/Home.vue
@@ -1,18 +1,57 @@
 <template>
-  <!--<v-container class="container__default">-->
-  <v-row>
-    <v-col v-if="this.$auth.loggedIn" cols="3" class="hidden-sm-and-down">
-      <ProfileWidget />
-      <InvitationLink />
-    </v-col>
-    <v-col cols="12" sm="12" :md="this.$auth.loggedIn ? 6 : 8">
-      <PostsFeed />
-    </v-col>
-    <v-col :cols="this.$auth.loggedIn ? 3 : 4" class="hidden-sm-and-down">
-      <SidebarWidgets />
-    </v-col>
-  </v-row>
-  <!--</v-container>-->
+  <div>
+    <v-row v-if="this.$auth.loggedIn">
+      <v-col cols="3" class="hidden-sm-and-down">
+        <ProfileWidget />
+        <InvitationLink />
+      </v-col>
+      <v-col cols="12" sm="12" :md="this.$auth.loggedIn ? 6 : 8">
+        <PostsFeed />
+      </v-col>
+      <v-col :cols="this.$auth.loggedIn ? 3 : 4" class="hidden-sm-and-down">
+        <SidebarWidgets />
+      </v-col>
+    </v-row>
+    <div v-else>
+      <v-container>
+        <v-row class="fill-height ma-0">
+          <v-col cols="6">
+            <v-img src="images/publichomepage.png" contain max-height="600" />
+          </v-col>
+          <v-col cols="6">
+            <v-card flat tile class="d-flex align-center" height="70%">
+              <h1>
+                Run your Digital
+                <p class="mb-0">Business with iBial</p>
+                <v-card-text>
+                  <v-row>
+                    <v-col class="pa-0">
+                      <v-btn
+                        rounded
+                        class="text-capitalize font-weight-regular py-6"
+                        color="primary"
+                        width="200"
+                      >
+                        Get Free Access
+                      </v-btn>
+                      <v-btn
+                        text
+                        class="text-capitalize font-weight-regular py-6"
+                        color="primary"
+                        width="150"
+                      >
+                        Learn more
+                      </v-btn>
+                    </v-col>
+                  </v-row>
+                </v-card-text>
+              </h1>
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
+    </div>
+  </div>
 </template>
 <script>
 import {
